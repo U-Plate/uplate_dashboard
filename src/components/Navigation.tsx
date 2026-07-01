dde3bedc3d6abf7fd16188b215d7ca2f58276593
Rishu Sharma <rishushar99@gmail.com>
2026-07-01 14:16:07 -0500

    added image approval

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -138,6 +138,16 @@ export const Navigation: React.FC = () => {
               Broadcast
             </NavLink>
           </li>
+          <li className="navigation__item">
+            <NavLink
+              to="/photo-review"
+              className={({ isActive }) =>
+                `navigation__link${isActive ? ' navigation__link--active' : ''}`
+              }
+            >
+              Photo Review
+            </NavLink>
+          </li>
         </ul>
       </div>
     </nav>
